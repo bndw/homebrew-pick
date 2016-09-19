58a9a49b9c9f5c5aafcc7e43cb985b03d1c10be9
Leon Klingele <git@leonklingele.de>
2016-09-19 02:20:06 +0200

    Initial packging of 0.2.1-alpha

---
+++ README.md
@@ -0,0 +1,10 @@
+# Homebrew packaging for [pick](https://github.com/bndw/pick)
+
+## pick — A minimal password manager for OS X and Linux
+
+### How to install
+
+```sh
+$ brew tap leonklingele/pick
+$ brew install leonklingele/pick/pick
+```
---
+++ pick.rb
@@ -0,0 +1,14 @@
+class Pick < Formula
+  desc "Minimal password manager for OS X and Linux"
+  homepage "https://github.com/bndw/pick"
+  url "https://github.com/bndw/pick/archive/v0.2.1.tar.gz"
+  version "0.2.1-alpha"
+  sha256 "4525565bdf711b8026a49beaa7ede6085378c1e046461ea244591234f61ec87e"
+
+  depends_on "go" => :build
+
+  def install
+    system "make"
+    bin.install "bin/pick"
+  end
+end
