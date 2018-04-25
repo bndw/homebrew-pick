4ccb6e16a948e5bd0d1097cc889a30da380b1a42
bndw <benjamindwoodward@gmail.com>
2018-04-25 14:44:56 -0700

    Version 0.7.0

--- pick-pass.rb
+++ pick-pass.rb
@@ -1,9 +1,9 @@
 class PickPass < Formula
   desc "Minimal password manager for OS X and Linux"
   homepage "https://github.com/bndw/pick"
-  url "https://github.com/bndw/pick/archive/v0.6.0.tar.gz"
-  version "0.6.0"
-  sha256 "d05fd35088d676c7e318a3c5eb98269232bd2815a6302fb3e222968edfa5dd56"
+  url "https://github.com/bndw/pick/archive/v0.7.0.tar.gz"
+  version "0.7.0"
+  sha256 "d9e2c99e7e49945fbe9ef27f5207a3f9709e047b18da73db2101a5445a676f45"
 
   depends_on "go" => :build
 
